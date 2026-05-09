a394b8f82b08fa70de3e18bf271310b2e0c5ab68
Sanjog Aale <sanjogm940@gmail.com>
2026-05-10 01:06:25 +0545

    html enitities

--- practice.js
+++ practice.js
@@ -7,8 +7,6 @@ function findFirst(array, func) {
   return undefined;
 }
 console.log(findFirst([1, 2, 3, 4], num => num % 2 === 0)); 
-console.log(findFirst([1, 2, 3, 4], num => num > 5));
-console.log(findFirst([1, 2, 3, 4], num => num < 0));
 console.log(findFirst([1, 2, 3, 4], num => num === 3));
 
 
@@ -190,3 +188,13 @@ function pairElement(str){
   }return result;
 }
 console.log(pairElement("ATG"));
+
+function convertHTML (str) {
+  return str
+   .replace(/&/g , "&amp;")
+   .replace(/</g , "&lt;")
+  .replace(/>/g , "&gt;")
+  .replace(/"/g , "&quot;")
+  .replace(/'/g , "&apos;");
+  }
+  console.log(convertHTML("Dol & Gabb"));
