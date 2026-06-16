49ed95f1f458cbf932a001224ee7e3e960665fb8
Sanjog Aale <sanjogm940@gmail.com>
2026-06-16 17:15:22 +0545

    folder structure & auth routes

--- project/client/app/page.tsx
+++ project/client/app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen w-full">
-      <h1>Home Page</h1>
+      {/* nav */}
+      <h1>Landing Page</h1>
+      {/* hero section */}
+      {/* Our category */}
+      {/* featured products */}
+      {/* new arrivals */}
+
+      {/* footer */}
     </main>
   )
 }
