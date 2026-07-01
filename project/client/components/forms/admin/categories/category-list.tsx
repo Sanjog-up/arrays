73e7aef1f1f52e97d375a82eb3eb9b9a1bcd87a8
Sanjog Aale <sanjogm940@gmail.com>
2026-07-01 22:41:22 +0545

    categroy list & table

--- project/client/components/forms/admin/categories/category-list.tsx
+++ project/client/components/forms/admin/categories/category-list.tsx
@@ -1,12 +1,99 @@
+'use client'
 import React from "react";
 import AdminListCard from "@/components/forms/admin/list-card";
+import { createColumnHelper } from "@tanstack/react-table";
+import Image from "next/image";
+import { CiTrash } from "react-icons/ci";
+import { MdEditDocument } from "react-icons/md";
+import Table from "../brands/table";
 
 const CategoryList = () => {
+
+    const defaultData= [
+      {
+        name: 'Category 1',
+        description: 'description category ',
+        logo:{
+          path:'/next.svg',
+          public_id:'/next.svg'
+        },
+        created_at: '07-01-2026',
+        updated_at: '07-01-2026',
+      },
+      {
+        name: 'Category 2',
+        description: 'description category ',
+        logo:{
+          path:'/next.svg',
+          public_id:'/next.svg'
+        },
+        created_at: '07-01-2026',
+        updated_at: '07-01-2026',
+      },
+    ]
+    
+    const columnHelper = createColumnHelper<any>()
+    
+    const columns = [
+      columnHelper.accessor((row)=> row.name, {
+        id:'name',
+        cell:(info) => <strong><i>{info.getValue()}</i></strong>,
+        header: () => <span className='text-lg'>Name</span>,
+      }),
+      columnHelper.accessor((row)=> row.logo, {
+        id:'logo',
+        cell:(info) => {
+         console.log(info.row.original.name)
+          return (
+            <div className="h-16 w-16 ">
+              <Image
+                src={info.getValue().path}
+                alt={`${info.row.original.name}-logo`}
+                width={100}
+                height={100}
+                className='object-contain h-full w-full'
+              />  
+            </div>
+          )
+        },
+        header: () => <span>Logo</span>,
+      }),
+      columnHelper.accessor((row)=> row.description, {
+        id:'description',
+        cell:(info) => <i>{info.getValue()}</i>,
+        header: () => <span>Description</span>,
+        footer: (info) => info.column.id,
+      }),
+      columnHelper.accessor((row)=> row.created_at, {
+        id:'created_at',
+        cell:(info) => <i>{info.getValue()}</i>,
+        header: () => <span>Created At</span>,
+        footer: (info) => info.column.id,
+      }),
+  
+          columnHelper.accessor((row)=> row, {
+        id:'_',
+        cell:(info) => <div className='flex gap-2 justify-center'>
+          <CiTrash title="Delete" className='text-red-500 text-[20px] cursor-pointer'/>
+          <MdEditDocument title="Edit" className='text-blue-500 text-[20px] cursor-pointer'/>
+        </div>,
+        header: () => <span>Actions</span>,
+     
+      }),
+    ]
+    
+    
   return (
     <AdminListCard>
       <h4 className="text-[18px] font-semibold text-black-500">
         Category list
       </h4>
+      <div className= 'w-full h-full rounded-sm'>
+        <Table
+          data={defaultData}
+          columns={columns}
+        />
+      </div>
     </AdminListCard>
   );
 };
